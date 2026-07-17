bc246b10eca53ceb90516e05e4868fe026cbdc0d
Antoni Przybylik <antoni@taon.io>
2026-07-17 17:21:05 +0200

    Updated tests `mod.rs` file.

--- mini-ode/src/tests/mod.rs
+++ mini-ode/src/tests/mod.rs
@@ -1,8 +1,8 @@
 mod compute_jacobian;
 mod mock_optimizer;
+mod ode_solving;
 mod optimizer_display;
 mod optimizers_derivative_tensors_123_helper;
 mod optimizers_differentiate_helper;
 mod optimizers_gradient_and_hessian_helper;
 mod solver_display;
-mod solvers;
